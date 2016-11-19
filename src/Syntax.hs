f19342835ee5980aec44ab06601d0c014e4350d9
Filippo Sestini <sestini.filippo@gmail.com>
2016-11-19 20:08:06 +0100

    Change Syntax

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -55,6 +55,5 @@ preludeDefs = [
   ("K1", ["x", "y"], EVar "y"),
   ("S", ["f", "g", "x"], EAp (EAp (EVar "f") (EVar "x")) (EAp (EVar "g") (EVar "x"))),
   ("compose", ["f", "g", "x"], EAp (EVar "f") (EAp (EVar "g") (EVar "x"))),
-  ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f")),
-  ("asd", [], ELet Recursive [("x", EVar "y"), ("z", ENum 10)] (ENum 100))
+  ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f"))
    ]
