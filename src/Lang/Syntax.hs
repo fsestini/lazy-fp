75c2f76f59dc4ec45b7ce77938473b8e9d8361a2
Filippo Sestini <sestini.filippo@gmail.com>
2016-11-25 12:30:58 +0100

    Add language syntax

---
+++ src/Lang/Syntax.hs
@@ -0,0 +1,34 @@
+module Lang.Syntax where
+
+type Name = String
+type TypeName = Name
+type CtorName = Name
+type AdtCtor = (CtorName, [TypeName])
+type AdtDecl = (TypeName, [AdtCtor])
+
+type Alter a = (CtorName, [a], Expr a)
+type LangAlter = Alter Name
+
+data LetMode = Recursive | NonRecursive deriving (Eq, Show)
+data BinOp = Plus | Minus | Mult | Div deriving (Eq, Show)
+
+data PrimOp = PrimComp | PrimSum | PrimSub | PrimMul | PrimDiv
+            deriving (Eq,Show)
+
+data Expr a = EVar Name
+            | ENum Int
+            | ECtor CtorName
+            | EAp (Expr a) (Expr a)
+            | ELet LetMode [(a, Expr a)] (Expr a)
+            | ECase (Expr a) [Alter a]
+            | ELam [a] (Expr a)
+            | EPrimitive PrimOp -- primitive operations
+            deriving (Eq, Show)
+
+type LangExpr = Expr Name
+
+type ScDefn a = (Name, [a], Expr a)
+type LangScDefn = ScDefn Name
+
+type Program a = ([AdtDecl],[ScDefn a])
+type LangProgram = Program Name
