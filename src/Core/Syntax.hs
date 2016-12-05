29417e820bdd3a090469725c071d57c97f024141
Filippo Sestini <sestini.filippo@gmail.com>
2016-12-05 02:14:28 +0100

    Add explicit exports in Core.Syntax

--- src/Core/Syntax.hs
+++ src/Core/Syntax.hs
@@ -1,9 +1,16 @@
-module Core.Syntax where
+module Core.Syntax (
+  CoreExpr(..),
+  freeVars,
+  allVars,
+  LetMode(..),
+  Name,
+  CtorName
+) where
 
 import Data.Set hiding (foldr, map)
 import Lang.Syntax(LangExpr(..), Lit(..), PrimOp, LetMode(..), Name, CtorName)
 import Control.Arrow (second)
-import Data.List.NonEmpty(NonEmpty(..), map, toList)
+import qualified Data.List.NonEmpty as NE (NonEmpty(..), map, toList)
 import Utils
 
 type CoreAlter a = (CtorName, [a], CoreExpr a)
@@ -13,8 +20,8 @@ data CoreExpr a = EVar a
             | ENum Int
             | ECtor CtorName
             | EAp (CoreExpr a) (CoreExpr a)
-            | ELet LetMode (NonEmpty (a, CoreExpr a)) (CoreExpr a)
-            | ECase (CoreExpr a) (NonEmpty (CoreAlter a))
+            | ELet LetMode (NE.NonEmpty (a, CoreExpr a)) (CoreExpr a)
+            | ECase (CoreExpr a) (NE.NonEmpty (CoreAlter a))
             | ELam a (CoreExpr a)
             | EPrimitive PrimOp -- primitive operations
             | EError
@@ -27,9 +34,9 @@ substituteVar u v (EVar x) | v == x = EVar u
 substituteVar u v (EAp e1 e2) =
   EAp (substituteVar u v e1) (substituteVar u v e2)
 substituteVar u v (ELet mode binds e) =
-  ELet mode (Data.List.NonEmpty.map (second (substituteVar u v)) binds) (substituteVar u v e)
+  ELet mode (NE.map (second (substituteVar u v)) binds) (substituteVar u v e)
 substituteVar u v (ECase e alts) =
-  ECase (substituteVar u v e) (Data.List.NonEmpty.map (third (substituteVar u v)) alts)
+  ECase (substituteVar u v e) (NE.map (third (substituteVar u v)) alts)
 substituteVar u v e@(ELam x body) | v == x = e
                                   | otherwise = ELam x (substituteVar u v body)
 substituteVar u v e = e
@@ -42,38 +49,40 @@ allVars (ECase e a) = allVars e `union` allVarsAlters a
 allVars (ELam x e) = singleton x `union` allVars e
 allVars _ = empty
 
-allVarsBinders :: Ord a => NonEmpty (a, CoreExpr a) -> Set a
-allVarsBinders b = fromList (Prelude.map fst bs) `union`
+allVarsBinders :: Ord a => NE.NonEmpty (a, CoreExpr a) -> Set a
+allVarsBinders b = fromList (map fst bs) `union`
   foldr (union . allVars . snd) empty bs
   where
-    bs = Data.List.NonEmpty.toList b
+    bs = NE.toList b
 
-allVarsAlters :: Ord a => NonEmpty (CtorName, [a], CoreExpr a) -> Set a
+allVarsAlters :: Ord a => NE.NonEmpty (CtorName, [a], CoreExpr a) -> Set a
 allVarsAlters a = fromList (concatMap (\(_,x,_) -> x) as) `union`
   foldr (union . (\(_,_,x) -> allVars x)) empty as
   where
-    as = Data.List.NonEmpty.toList a
+    as = NE.toList a
 
 freeVars :: Ord a => CoreExpr a -> Set a
-freeVars = undefined
--- freeVars (EVar x) = fromList [x]
--- freeVars (ENum e) = empty
--- freeVars (ECtor e) = empty
--- freeVars (EAp e1 e2) = freeVars e1 `union` freeVars e2
--- freeVars (ELet Recursive b e) =
---   (freeVarsBinders b `difference` binderVars)
---   `union` (freeVars e `difference` binderVars)
---   where
---     binderVars = fromList $ Prelude.map fst $ Data.List.NonEmpty.toList b
--- freeVars (ELet NonRecursive b e) =
---   freeVarsBinders b `union` (freeVars e `difference` binderVars)
---   where
---     binderVars = fromList $ Prelude.map fst $ Data.List.NonEmpty.toList b
--- freeVars (ECase e a) = undefined
--- freeVars (ELam e1 e2) = undefined
--- freeVars (EPrimitive e) = undefined
--- freeVars EError = undefined
---
--- freeVarsBinders :: Ord a => NonEmpty (a, CoreExpr a) -> Set a
--- freeVarsBinders xs = foldr union empty $
---   Data.List.NonEmpty.map (snd . second freeVars) xs
+freeVars (EVar x) = fromList [x]
+freeVars (EAp e1 e2) = freeVars e1 `union` freeVars e2
+freeVars (ELet m b e) =
+  freeVarsB `union` (freeVars e `difference` binderVars)
+  where
+    binderVars = fromList . NE.toList $ NE.map fst b
+    freeVarsB = case m of
+      Recursive -> freeVarsBinders b `difference` binderVars
+      NonRecursive -> freeVarsBinders b
+freeVars (ECase e a) = freeVars e `union` freeVarsAlters
+  where
+    freeVarsAlters = foldr union empty (NE.map freeVarsAlter a)
+freeVars (ELam x e) = freeVars e `difference` singleton x
+freeVars (ENum e) = empty
+freeVars (ECtor e) = empty
+freeVars (EPrimitive _) = empty
+freeVars EError = empty
+
+freeVarsAlter :: (CtorName, [a], CoreExpr a) -> Set a
+freeVarsAlter = undefined
+
+freeVarsBinders :: Ord a => NE.NonEmpty (a, CoreExpr a) -> Set a
+freeVarsBinders xs = foldr union empty $
+  NE.map (snd . second freeVars) xs
