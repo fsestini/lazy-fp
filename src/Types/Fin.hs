9a1b3614c53b5b9e72b5efd9e8433e5d1053cb77
Filippo Sestini <sestini.filippo@gmail.com>
2016-12-24 15:09:46 +0100

    Add type of finite sets

---
+++ src/Types/Fin.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE StandaloneDeriving, DataKinds, GADTs, KindSignatures #-}
+
+module Types.Fin where
+
+data Nat = Zero | Succ Nat
+
+data Fin :: Nat -> * where
+  FZero :: Fin (Succ n)
+  FSucc :: Fin n -> Fin (Succ n)
+
+deriving instance Eq (Fin n)
+deriving instance Ord (Fin n)
+deriving instance Show (Fin n)
+
+data NNat :: Nat -> * where
+  NZero :: NNat Zero
+  NSucc :: NNat n -> NNat (Succ n)
+
+raiseFin :: Fin n -> Fin (Succ n)
+raiseFin FZero = FZero
+raiseFin (FSucc fin) = FSucc (raiseFin fin)
+
+maxOfFin :: NNat n -> Fin (Succ n)
+maxOfFin NZero = FZero
+maxOfFin (NSucc nn) = FSucc (maxOfFin nn)
+
+thick :: NNat n -> Fin (Succ n) -> Fin (Succ n) -> Maybe (Fin n)
+thick NZero FZero FZero = Nothing
+thick NZero FZero (FSucc f2) = Just f2
+thick (NSucc _) FZero FZero = Nothing
+thick (NSucc _) FZero (FSucc f2) = Just f2
+thick (NSucc _) (FSucc _) FZero = Just FZero
+thick (NSucc n) (FSucc f1) (FSucc f2) = fmap FSucc (thick n f1 f2)
