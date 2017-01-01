639bc555226d5fce91d0f0fdbec5f4bac5ff89d9
Filippo Sestini <sestini.filippo@gmail.com>
2017-01-02 00:03:54 +0100

    Upgrade GMachine.Syntax

--- src/GMachine/Syntax.hs
+++ src/GMachine/Syntax.hs
@@ -1,72 +1,55 @@
 {-# LANGUAGE PatternSynonyms, GeneralizedNewtypeDeriving, TypeOperators,
-             TemplateHaskell #-}
+             TemplateHaskell, LiberalTypeSynonyms, DeriveFunctor,
+             DeriveFoldable, DeriveTraversable, FlexibleContexts,
+             FlexibleInstances, MultiParamTypeClasses #-}
 
 module GMachine.Syntax (
   Name, CtorTag, CtorArity, LetMode(..), PrimOp(..), Lit(..),
-  GExpr(..), bindersOf, rhssOf,
-  GAlterB(..), GAlter, GBinders,
+  GExpr(..), GAlterB(..), GAlter, GBinders,
   isAtomicExpr,
   pattern GVarF, pattern GLitF, pattern GPackF, pattern GAppF,
   pattern GLetF, pattern GPrimF, pattern GVar, pattern GLit, pattern GPack,
   pattern GApp, pattern GLet, pattern GPrim,
   GScDefn, GProgram, GProgramN, GExprN, GScDefnN, GAlterN, GBindersN,
   preludeDefs,
-  GSelB(..), GCaseB(..)
+  GSelB(..), GCaseB(..), iGCase
 ) where
 
 import qualified Data.List.NonEmpty as NE
+import qualified Data.Set as S
 import AssocList
 import AST
-import RecursionSchemes
 import Data.Bifunctor
 import Data.Bifunctor.TH
 import Data.Bifoldable
 import Data.Bitraversable
+import Data.Comp.Bifun
 
 type CtorTag = Int
 
 data GPackB a b = GPackB CtorTag CtorArity
   deriving (Eq, Show, Ord)
-$(deriveBifunctor ''GPackB)
-$(deriveBifoldable ''GPackB)
-$(deriveBitraversable ''GPackB)
+
+iGCase :: (GCaseB :<: f) => Int -> Term f a
+iGCase = inject . GCaseB
 
 data GCaseB a b = GCaseB Int
-  deriving(Eq, Ord, Show)
-$(deriveBifunctor ''GCaseB)
-$(deriveBifoldable ''GCaseB)
-$(deriveBitraversable ''GCaseB)
+  deriving (Eq, Show, Ord)
 
 data GSelB a b = GSelB CtorArity Int
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Show, Ord)
+
 $(deriveBifunctor ''GSelB)
+$(deriveBifunctor ''GCaseB)
 $(deriveBifoldable ''GSelB)
+$(deriveBifoldable ''GCaseB)
 $(deriveBitraversable ''GSelB)
+$(deriveBitraversable ''GCaseB)
 
-newtype GExprBase a b = GEB ((
-    VarB :++: LitB :++: GPackB :++: AppB :++: LetB :++: PrimB
-    :++: GSelB :++: GCaseB
-  ) a b) deriving (Bifunctor, Bifoldable)
+type GExprBaseB = VarB :+: LitB :+: GPackB :+: AppB
+                  :+: LetB :+: PrimB :+: GSelB :+: GCaseB
 
-instance Bitraversable GExprBase where
-  bitraverse f g (GEB x) = GEB <$> bitraverse f g x
-
-type GExpr = FixB GExprBase
-
--- data GExpr a = GVar Name
---              | GLit Lit
---              | GPack CtorTag CtorArity
---              | GApp (GExpr a) (GExpr a)
---              | GLet LetMode (NE.NonEmpty (BinderB a (GExpr a))) (GExpr a)
---              | GCase (GExpr a) [GAlterB a (GExpr a)]
---              | GPrim PrimOp
---              deriving (Eq, Show)
-
-bindersOf :: NE.NonEmpty (a,b) -> NE.NonEmpty a
-bindersOf = NE.map fst
-
-rhssOf :: NE.NonEmpty (a,b) -> NE.NonEmpty b
-rhssOf = NE.map snd
+type GExpr = Term GExprBaseB
 
 -- case expressions have an expression to analyze, and a list of alternatives.
 -- Each alternative contains a tag, a list of the bound variables and the
@@ -107,21 +90,22 @@ preludeDefs = [
 --------------------------------------------------------------------------------
 -- Pattern definitions
 
-pattern GVarF x        = (GEB (Lb (VarB x)))
-pattern GLitF x        = (GEB (Rb (Lb (LitB x))))
-pattern GPackF x1 x2   = (GEB (Rb (Rb (Lb (GPackB x1 x2)))))
-pattern GAppF x1 x2    = (GEB (Rb (Rb (Rb (Lb (AppB x1 x2))))))
-pattern GLetF x1 x2 x3 = (GEB (Rb (Rb (Rb (Rb (Lb (LetB x1 x2 x3)))))))
-pattern GPrimF x       = (GEB (Rb (Rb (Rb (Rb (Rb (Lb (PrimB x))))))))
-pattern GSelF a i      = (GEB (Rb (Rb (Rb (Rb (Rb (Rb (Lb (GSelB a i)))))))))
-pattern GCaseF t       = (GEB (Rb (Rb (Rb (Rb (Rb (Rb (Rb (GCaseB t)))))))))
+pattern GVarF x        = (Inl (VarB x))
+pattern GLitF x        = (Inr (Inl (LitB x)))
+pattern GPackF x1 x2   = (Inr (Inr (Inl (GPackB x1 x2))))
+pattern GAppF x1 x2    = (Inr (Inr (Inr (Inl (AppB x1 x2)))))
+pattern GLetF x1 x2 x3 = (Inr (Inr (Inr (Inr (Inl (LetB x1 x2 x3))))))
+pattern GPrimF x       = (Inr (Inr (Inr (Inr (Inr (Inl (PrimB x)))))))
+pattern GSelF a i      = (Inr (Inr (Inr (Inr (Inr (Inr (Inl (GSelB a i))))))))
+pattern GCaseF t       = (Inr (Inr (Inr (Inr (Inr (Inr (Inr (GCaseB t))))))))
 
-pattern GVar x        = FixB (GEB (Lb (VarB x)))
-pattern GLit x        = FixB (GEB (Rb (Lb (LitB x))))
-pattern GPack x1 x2   = FixB (GEB (Rb (Rb (Lb (GPackB x1 x2)))))
-pattern GApp x1 x2    = FixB (GEB (Rb (Rb (Rb (Lb (AppB x1 x2))))))
-pattern GLet x1 x2 x3 = FixB (GEB (Rb (Rb (Rb (Rb (Lb (LetB x1 x2 x3)))))))
-pattern GPrim x       = FixB (GEB (Rb (Rb (Rb (Rb (Rb (Lb (PrimB x))))))))
-pattern GSel a i
-  = FixB (GEB (Rb (Rb (Rb (Rb (Rb (Rb (Lb (GSelB a i)))))))))
-pattern GCase t       = FixB (GEB (Rb (Rb (Rb (Rb (Rb (Rb (Rb (GCaseB t)))))))))
+pattern GVar x        = Term (Inl (VarB x))
+pattern GLit x        = Term (Inr (Inl (LitB x)))
+pattern GPack x1 x2   = Term (Inr (Inr (Inl (GPackB x1 x2))))
+pattern GApp x1 x2    = Term (Inr (Inr (Inr (Inl (AppB x1 x2)))))
+pattern GLet x1 x2 x3 = Term (Inr (Inr (Inr (Inr (Inl (LetB x1 x2 x3))))))
+pattern GPrim x       = Term (Inr (Inr (Inr (Inr (Inr (Inl (PrimB x)))))))
+pattern GSel a i      =
+  Term (Inr (Inr (Inr (Inr (Inr (Inr (Inl (GSelB a i))))))))
+pattern GCase t       =
+  Term (Inr (Inr (Inr (Inr (Inr (Inr (Inr (GCaseB t))))))))
